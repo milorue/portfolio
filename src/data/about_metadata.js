5b600726508621151ed67c049ba18b2014c5b677
mrue <milorue@gmail.com>
2020-07-17 10:21:58 -0400

    css changes to nav navigation

--- src/data/about_metadata.js
+++ src/data/about_metadata.js
@@ -1,3 +1,3 @@
-const about = "I'm a React, NodeJS, GraphQL, & MongoDB Full-Stack Web Developer who has recently finished a degree in Computer Science at Ithaca College. I enjoy hiking, camping, computers, and pets and I have a 6 year old rescue cat named Siggy. I am like working in fast-paced and interesting full-stack web and/or mobile development environment. I also enjoy working on low level hardware and data solutions."
+const about = "I'm a React, NodeJS, GraphQL, & MongoDB Full-Stack Web Developer who has recently finished a degree in Computer Science at Ithaca College. I enjoy hiking, camping, computers, and pets and I have a 6 year old rescue cat named Siggy. I like working in fast-paced and interesting full-stack web and/or mobile development environment. I also enjoy working on low level hardware and data solutions."
 
 export default about
