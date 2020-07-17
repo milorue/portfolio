72fdf6e78012a66f279c8c3110500b1095959e8e
mrue <milorue@gmail.com>
2020-07-16 22:27:43 -0400

    added linking and repo buttons

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -30,17 +30,37 @@ function Projects(props){
         }
     })
 
+    const renderLinkButtons = (links) => {
+        if(links.length > 0){
+            return links.map((link) => {
+                return(
+                    <Button style={{color: props.theme.fontColor, fontSize: 9, overflow: 'hidden'}} href={link} type={"text"} size={'middle'}>{<DesktopOutlined style={{color: props.theme.fontColor}}/>} {link}</Button>
+                )
+            })
+        }
+    }
+
+    const renderRepoButtons = (repos) => {
+        if(repos.length > 0){
+            return repos.map((repos, index) => {
+                return(
+                <Button style={{color: props.theme.fontColor, fontSize: 9, overflow: 'hidden'}} href={repos} type={"text"} size={'middle'}>{<GithubOutlined style={{color: props.theme.fontColor}}/>} Github Repo: {index+1}</Button>
+                )
+            })
+        }
+    }
+
     const renderProjects = () => {
         if(projects.length > 0){
             return projects.map((project) => {
                 return(
-                    <Col xs={24} sm={12} md={8} lg={6} key={project.name} style={{width: "100%"}}>
+                    <Col xs={24} sm={12} md={12} lg={8} xl={6} xxl={4} key={project.name} style={{width: "100%"}}>
                         <Card style={{borderColor: 'transparent', borderRadius: 5, borderWidth: 0}} cover={<img style={{color: props.theme.fontColor ,height: 200, width: "100%"}} className={"cardImg"} src={project.image}/>} bodyStyle={{borderColor: "transparent", height: 250, textAlign: 'left' ,backgroundColor: props.theme.boxColor, color: props.theme.fontColor}} hoverable>
                             <h4 style={{color: props.theme.fontColor}}>{project.name}</h4>
-                            <Typography.Paragraph ellipsis={{rows: 3}} style={{fontSize: 10}}>{project.description}</Typography.Paragraph>
+                            <Typography.Paragraph ellipsis={{rows: 3}} style={{fontSize: 10, color: props.theme.fontColor}}>{project.description}</Typography.Paragraph>
                             <Row style={{marginTop: 10}}>
-                                <Button href={project.links[0]} type={"text"} size={'large'}>{<DesktopOutlined style={{color: props.theme.fontColor}}/>}</Button>
-                                <Button href={project.repos[0]}type={"text"} size={'large'}>{<GithubOutlined style={{color: props.theme.fontColor}}/>}</Button>
+                                {renderLinkButtons(project.links)}
+                                {renderRepoButtons(project.repos)}
                             </Row>
                         </Card>
                     </Col>
